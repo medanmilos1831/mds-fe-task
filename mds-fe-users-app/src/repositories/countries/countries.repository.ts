dc151525702ddd5c5ac6aade2fa0883cec09b5e6
medanmilos1831 <medanmilos1831@gmail.com>
2025-12-19 23:23:15 +0100

    add repository CRUD methods

--- mds-fe-users-app/src/repositories/countries/countries.repository.ts
+++ mds-fe-users-app/src/repositories/countries/countries.repository.ts
@@ -1,8 +1,12 @@
-import type { ApiClientType } from "@/types";
+import { API_URL_ROUTES, type ApiClientType } from "@/types";
+import type { ICountry } from "./types";
 
 const createCountryRepository = (api: ApiClientType) => {
-  console.log("api", api);
-  return {};
+  return {
+    getCountries: () => {
+      return api.get<ICountry[]>(API_URL_ROUTES.COUNTRIES);
+    },
+  };
 };
 
 export { createCountryRepository };
--- mds-fe-users-app/src/repositories/user/user.repository.ts
+++ mds-fe-users-app/src/repositories/user/user.repository.ts
@@ -1,8 +1,24 @@
-import type { ApiClientType } from "@/types";
+import {
+  API_URL_ROUTES,
+  type ApiClientType,
+  type IResponseWithPagination,
+} from "@/types";
+import type { IUser } from "./types";
 
 const createUserRepository = (api: ApiClientType) => {
-  console.log("api", api);
-  return {};
+  return {
+    getUsers: (params: any) => {
+      return api.get<IResponseWithPagination<IUser>>(API_URL_ROUTES.USERS, {
+        params,
+      });
+    },
+    getUser: (id: number) => {
+      return api.get<IUser>(`${API_URL_ROUTES.USERS}/${id}`);
+    },
+    removeUser: (id: number) => {
+      return api.remove<void>(`${API_URL_ROUTES.USERS}/${id}`);
+    },
+  };
 };
 
 export { createUserRepository };
